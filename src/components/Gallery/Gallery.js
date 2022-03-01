e1957066338d6a6a0ae032f864c226db83d8057e
choiree <riririgood@naver.com>
2022-03-01 17:15:11 +0900

    feature: Main Page

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GalleryWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #1e1e1e;
+`;
+
+export const Gallery = () => {
+  return <GalleryWrapper>Gallery</GalleryWrapper>;
+};
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,60 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import { getUserInfo } from '../../api/user';
 import { saveLoginUser } from '../../features/user/userSlice';
 import { Login } from '../Login/Login';
 import useAuth from './useAuth';
 
+const MainWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+const MainLeft = styled.div`
+  width: 50vw;
+  height: 100vh;
+  background-color: #2b3255;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 400px;
+    height: 300px;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+  }
+
+  i {
+    font-size: 250px;
+    color: #2b3255;
+    cursor: pointer;
+  }
+`;
+
+const MainRight = styled.div`
+  width: 50vw;
+  height: 100vh;
+  background-color: #ffcd48;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 150px;
+
+  div {
+    cursor: pointer;
+  }
+`;
+
 export const Main = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const code = new URLSearchParams(window.location.search).get('code');
   const accessToken = window.localStorage.getItem('jwtAccessToken');
 
@@ -30,5 +78,28 @@ export const Main = () => {
     userInfo();
   }, []);
 
-  return accessToken ? <div>main</div> : <Login />;
+  return accessToken ? (
+    <MainWrapper>
+      <MainLeft>
+        <div
+          onClick={() => {
+            navigate('/image');
+          }}
+        >
+          <i className="fas fa-solid fa-image" />
+        </div>
+      </MainLeft>
+      <MainRight>
+        <div
+          onClick={() => {
+            navigate('/emoji');
+          }}
+        >
+          😻
+        </div>
+      </MainRight>
+    </MainWrapper>
+  ) : (
+    <Login />
+  );
 };
